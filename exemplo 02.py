500287f07a2df70dc7161e484ef2913963fa6637
larahcorrea <111812003+larahcorrea@users.noreply.github.com>
2022-08-23 20:29:50 -0300

    add ex 2 e 3

---
+++ exemplo 02.py
@@ -0,0 +1,3 @@
+l = int ( input ('Digite o lado: ' ))
+a = l * l
+print  ('A área é ', a )
---
+++ exemplo03.py
@@ -0,0 +1,7 @@
+b = int ( input ('Digite a base: ' ))
+a = int ( input ('Digite a altura: ' ))
+area = ( a * b ) / 2
+print('A área do triângulo é: ', area )
+
+
+
