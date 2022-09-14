0e537b2c2545eb58cae7a348081474401a040462
larahcorrea <111812003+larahcorrea@users.noreply.github.com>
2022-09-13 22:40:46 -0300

    Add files via upload
    ultimos

---
+++ ex48.py
@@ -0,0 +1,22 @@
+resposta = 'S'
+
+while(resposta == 'S'):
+
+    num = int(input('Deseja calcular o fatorial de qual número? '))
+
+    while(num <= 0):
+        print('Opa! Não pode número negativo!')
+        num = int(input('Deseja calcular o fatorial de qual número? '))
+
+
+    for i in range(num - 1, 0, -1):
+        resultado = num * i
+        num = resultado
+        
+        
+    print('Fatorial: ', resultado)
+    resposta = input('Deseja uma nova execução do programa?(S/N) ').upper()
+
+    while(resposta != 'S' and resposta != 'N'):
+        print('Seguir padrão de resposta S/N')
+        resposta = input('Deseja uma nova execução do programa?(S/N) ').upper()
